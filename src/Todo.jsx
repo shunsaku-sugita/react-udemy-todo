4ce0595a5099c2ff137fb08d8feedba3340d6713
shunsaku-sugita <saku.dot@gmail.com>
2024-06-05 22:26:13 -0700

    Add structures

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,8 +1,43 @@
+import { useState } from "react";
+import "./style.css";
+
 export const Todo = () => {
+  const [todoText, setTodoText] = useState("aaa");
+  const [incompleteTodos, setIncompleteTodos] = useState(["TODO1", "TODO2"]);
+  const [completeTodos, setCompleteTodos] = useState(["TODO1", "TODO2"]);
   return (
-    <div className="App">
-      <h1>Hello</h1>
-      <h2>How are you</h2>
-    </div>
+    <>
+      <div className="input-area">
+        <input placeholder="Input a TODO" value={todoText} />
+        <button>Add</button>
+      </div>
+      <div className="incomplete-area">
+        <p className="title">Incomplete TODOs</p>
+        <ul>
+          {incompleteTodos.map((todo) => (
+            <li key={todo}>
+              <div className="list-row">
+                <p className="todo-item">{todo}</p>
+                <button>Complete</button>
+                <button>Delete</button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="complete-area">
+        <p className="title">Complete TODOs</p>
+        <ul>
+          {completeTodos.map((todo) => (
+            <li key={todo}>
+              <div className="list-row">
+                <p className="todo-item">{todo}</p>
+                <button>Go Back</button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 };
